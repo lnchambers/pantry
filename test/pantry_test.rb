bb9f4d58689f832b0aeda0255c9ac575ccde5923
Luke Chambers <lukenatchambers@gmail.com>
2018-01-09 09:28:05 -0700

    Create test for recipe/pantry interaction

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,4 +1,5 @@
 require './lib/pantry'
+require './lib/recipe'
 require 'minitest/autorun'
 require 'minitest/pride'
 
@@ -35,6 +36,20 @@ class PantryTest < Minitest::Test
     pantry.restock("Cheese", 20)
 
     assert_equal 30, pantry.stock_check("Cheese")
+  end
+
+  def test_recipe_can_be_added_to_shopping_list
+    recipe = Recipe.new("Cheese Pizza")
+    pantry = Pantry.new
+
+    recipe.add_ingredient("Flour", 500)
+    recipe.add_ingredient("Cheese", 1500)
+    pantry.add_to_shopping_list(recipe)
+
+    assert_instace_of Hash, pantry.shopping_list
+    assert_equal 2, pantry.shopping_list.count
+    assert_equal ["Flour", "Cheese"], pantry.shopping_list.keys
+    assert_equal [500, 1500], pantry.shopping_list.values
   end
 
 end
